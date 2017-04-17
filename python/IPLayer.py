7bce5961f9cc7ff5fc2242c9011c9b63f4f4a1b9
Eduardo Gonzalez <edugg89@gmail.com>
2017-04-17 22:22:16 +0200

    pruebay

---
+++ python/IPLayer.py
@@ -0,0 +1,20 @@
+'''
+Created on 14 abr. 2017
+
+@author: Edu
+'''
+
+def IP_handler():
+    print("IPhandler")
+
+class IP(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
